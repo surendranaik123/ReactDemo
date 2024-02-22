e126ad8d2c01b600c037f1d4d0db8434b9e0bce5
surendranaik123 <131369740+surendranaik123@users.noreply.github.com>
2024-02-22 11:31:46 +0530

    cha

--- backend/index.js
+++ backend/index.js
@@ -11,23 +11,17 @@ const PORT = process.env.PORT || 9000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(
 
-// Connect to MongoDB
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//     // Start the server after successfully connecting to MongoDB
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   })
-//   .catch(error => {
-//     console.error('Error connecting to MongoDB: ', error);
-//   });
+  {
+    origin:["https://website-delta-two-63.vercel.app"],
+    methods:["Post","Get"],
+    credentials:true 
+
+}
+));
+
+
 
 
 // Connect to MongoDB
